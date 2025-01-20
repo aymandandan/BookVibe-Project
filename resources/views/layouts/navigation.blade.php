4b14fdb4f53c67c9740aa6cc19ab991dad0a5b5e
AymanAD <aymandandan24680@gmail.com>
2025-01-20 19:45:45 +0200

    search bar added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false, sidebar_open: false }" class="bg-indigo-600 border-b border-gray-100 z-10 overflow-hidden">
+<nav x-data="{ open: false, sidebar_open: false }" class="bg-indigo-600 border-b border-gray-100 z-10">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -38,7 +38,26 @@
             </div>
 
             <!-- Search Bar -->
-
+            <div class="w-full mx-4 max-w-sm sm:flex justify-between items-center hidden">
+                <form class="flex w-full justify-between items-center flex-row gap-6">
+                    <input
+                        class="w-full h-8 rounded-lg bg-grey-200 border-s-primary-300 shadow border-grey-300 hover:bg-grey-100 focus:bg-grey-100 focus:outline-none transition ease-in-out duration-150"
+                        type="text" name="search_input" placeholder="Search ..." />
+                    <button
+                        class="inline-flex items-center px-1 py-1 border border-transparent shadow text-sm leading-4 font-medium rounded-md text-grey-900 bg-grey-200 hover:text-grey-800 hover:bg-grey-100 focus:outline-none transition ease-in-out duration-150">
+                        <div>
+                            <svg class="fill-current h-6 w-6" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg" transform="matrix(-1, 0, 0, 1, 0, 0)">
+                                <g id="SVGRepo_iconCarrier">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M10 5C7.23858 5 5 7.23858 5 10C5 12.7614 7.23858 15 10 15C11.381 15 12.6296 14.4415 13.5355 13.5355C14.4415 12.6296 15 11.381 15 10C15 7.23858 12.7614 5 10 5ZM3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 11.5719 16.481 13.0239 15.6063 14.1921L20.7071 19.2929C21.0976 19.6834 21.0976 20.3166 20.7071 20.7071C20.3166 21.0976 19.6834 21.0976 19.2929 20.7071L14.1921 15.6063C13.0239 16.481 11.5719 17 10 17C6.13401 17 3 13.866 3 10Z"
+                                        fill="#000000"></path>
+                                </g>
+                            </svg>
+                        </div>
+                    </button>
+                </form>
+            </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -122,7 +141,46 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
+    <!-- Responsive Navigation Bar -->
+    <div class="sm:hidden max-w-7xl mx-auto pb-1.5 px-2 flex">
+        <!-- Sidebar Button -->
+        <div class="me-2 flex items-center sm:hidden">
+            <button @click="sidebar_open = ! sidebar_open"
+                class="inline-flex items-center justify-center p-2 rounded-md text-gray-200 hover:text-gray-300 hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500 focus:text-gray-300 transition duration-150 ease-in-out">
+                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path :class="{ 'hidden': sidebar_open, 'inline-flex': !sidebar_open }" class="inline-flex"
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+
+                    <path :class="{ 'hidden': !sidebar_open, 'inline-flex': sidebar_open }" class="hidden"
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+
+        <!-- Search Bar -->
+        <div class="w-full mx-4 flex justify-between items-center">
+            <form class="flex w-full justify-between items-center flex-row gap-6">
+                <input
+                    class="w-full h-8 rounded-lg bg-grey-200 border-s-primary-300 shadow border-grey-300 hover:bg-grey-100 focus:bg-grey-100 focus:outline-none transition ease-in-out duration-150"
+                    type="text" name="search_input" placeholder="Search ..." />
+                <button
+                    class="inline-flex items-center px-1 py-1 border border-transparent shadow text-sm leading-4 font-medium rounded-md text-grey-900 bg-grey-200 hover:text-grey-800 hover:bg-grey-100 focus:outline-none transition ease-in-out duration-150">
+                    <div>
+                        <svg class="fill-current h-6 w-6" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" transform="matrix(-1, 0, 0, 1, 0, 0)">
+                            <g id="SVGRepo_iconCarrier">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M10 5C7.23858 5 5 7.23858 5 10C5 12.7614 7.23858 15 10 15C11.381 15 12.6296 14.4415 13.5355 13.5355C14.4415 12.6296 15 11.381 15 10C15 7.23858 12.7614 5 10 5ZM3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 11.5719 16.481 13.0239 15.6063 14.1921L20.7071 19.2929C21.0976 19.6834 21.0976 20.3166 20.7071 20.7071C20.3166 21.0976 19.6834 21.0976 19.2929 20.7071L14.1921 15.6063C13.0239 16.481 11.5719 17 10 17C6.13401 17 3 13.866 3 10Z"
+                                    fill="#000000"></path>
+                            </g>
+                        </svg>
+                    </div>
+                </button>
+            </form>
+        </div>
+    </div>
+
+    <!-- Responsive Settings Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden ">
         <!--
         <div class="pt-2 pb-3 space-y-1">
