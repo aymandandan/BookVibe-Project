a34fbcc4b12027c4dc6875ce1be0f02c7eb8dc24
AymanAD <aymandandan24680@gmail.com>
2025-02-03 21:24:45 +0200

    home merged

--- routes/home.php
+++ routes/home.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 
 use App\Http\Controllers\homecontroller;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Welcomecontroller;
 use Illuminate\Support\Facades\Route;
-Route::get('/home', [homecontroller::class,'index'])->name('home.index');
+Route::get('/', [homecontroller::class, 'index'])->name('home');
 Route::get('/about', [homecontroller::class, 'about']);
