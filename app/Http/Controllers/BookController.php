0084dd53e783fc84933bcbbb01e513c0d524b6df
AymanAD <aymandandan24680@gmail.com>
2025-02-19 21:08:24 +0200

    covers moved to public

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,7 +11,7 @@ use Storage;
 
 class BookController extends Controller
 {
-    private $defaultCover = 'storage/assets/pictures/BookCovers/Default_Img_Cover.jpg';
+    private $defaultCover = 'assets/pictures/BookCovers/Default_Img_Cover.jpg';
 
     /**
      * Display a listing of the resource.
@@ -215,7 +215,7 @@ class BookController extends Controller
                 $this->deleteFileIfExists($book->cover_img);
             }
 
-            return 'storage/' . $path;
+            return $path;
         }
 
         return $book->cover_img ?? $this->defaultCover;
@@ -242,7 +242,7 @@ class BookController extends Controller
                 $this->deleteFileIfExists($book->file_path);
             }
 
-            return 'storage/' . $path;
+            return $path;
         }
 
         return $book->file_path ?? null;
@@ -254,11 +254,8 @@ class BookController extends Controller
             return;
         }
 
-        // Remove 'storage/' prefix to get correct path
-        $relativePath = str_replace('storage/', '', $path);
-
-        if (Storage::disk('public')->exists($relativePath)) {
-            Storage::disk('public')->delete($relativePath);
+        if (Storage::disk('public')->exists($path)) {
+            Storage::disk('public')->delete($path);
         }
     }
 }
